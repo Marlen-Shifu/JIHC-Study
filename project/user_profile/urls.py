7c8d4a57a2e34190ea456d25f66e1ae45dc7cbf5
Marlen-Shifu <jfss7632@gmail.com>
2021-03-07 15:42:32 +0600

    ispravleno

--- project/user_profile/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('', profile, name='profile'),
-    path('login/', login_user, name='login'),
-    path('register/', register_user, name='register'),
-]
